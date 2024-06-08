5cc873dae39dbdd952e12cb5dc6b58cc67ce4f6e
brousermanager <bro.user.info@gmail.com>
2024-06-08 13:56:32 +0200

    colasantoluigi.it

---
+++ src/components/sections/IconText.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <br><h1 :style="{ color: info.primaryColor }">
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-icon icon="mdi-food-steak" :color="info.primaryColor" style="float: left;" />
+          <p class="contact__text">&nbsp;&nbsp;Macelleria</p>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-icon icon="mdi-food-drumstick-outline" :color="info.primaryColor" style="float: left;" />
+          <p class="contact__text">&nbsp;&nbsp;Salumeria</p>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-icon icon="mdi-egg-outline" :color="info.primaryColor" style="float: left;" />
+          <p class="contact__text">&nbsp;&nbsp;Polleria</p>
+        </v-col>
+      </v-row>
+    </h1>
+  </v-container>
+</template>
+
+<script setup>
+  const { content, info } = defineProps(['content', 'info']);
+</script>
--- src/components/sections/MapViewer.vue
+++ src/components/sections/MapViewer.vue
@@ -113,7 +113,7 @@ onMounted(async () => {
       anchor: [0.5, 46],
       anchorXUnits: 'fraction',
       anchorYUnits: 'pixels',
-      src: '/map.png'
+      src: 'image.png'
     })
   });
 
