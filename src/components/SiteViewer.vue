36048a2397604b54de90cbc93a28b5753c1b57f6
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-07-11 20:18:24 +0200

    fix di vanni

--- src/components/SiteViewer.vue
+++ src/components/SiteViewer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card class="styled-card" elevation="4">
+    <v-card elevation="4" height="500">
       <v-card-title class="title-card">
         <v-icon class="mr-2">mdi-web</v-icon>
         Siti Attivi
@@ -9,7 +9,7 @@
         <v-container class="scrollable-container">
           <v-row>
             <v-col v-for="site in content" :key="site.name" cols="12" md="6">
-              <v-card class="site-card mb-1" :style="{ borderColor: info.primaryColor }" elevation="2">
+              <v-card class="site-card" :style="{ borderColor: info.primaryColor }" elevation="2">
                 <v-card-title class="d-flex align-center justify-space-between py-2">
                   <span class="site-name text-body-2">{{ site.name }}</span>
                   <div class="d-flex align-center">
@@ -35,16 +35,7 @@
 </script>
 
 <style scoped>
-.styled-card { 
-  overflow: hidden;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-  max-height: 500px;
-}
-
 .title-card {
-  display: flex;
-  align-items: center;
-  font-size: 1.25rem;
   font-weight: bold;
   color: #3f51b5;
   background-color: #e3f2fd;
@@ -75,7 +66,6 @@
 .site-card {
   border: 2px solid;
   border-radius: 8px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s, box-shadow 0.2s;
 }
 
@@ -84,20 +74,11 @@
   box-shadow: 0 4px 16px rgba(0, 0, 0, 0.2);
 }
 
-.v-btn {
-  font-size: 0.875rem;
-}
-
 .v-avatar {
   border: 2px solid #eee;
 }
 
 .site-name {
   font-weight: bold;
-  color: rgb(0, 0, 0);
-}
-
-.mb-1 {
-  margin-bottom: 4px; /* Riduce ulteriormente lo spazio tra le righe */
 }
 </style>
