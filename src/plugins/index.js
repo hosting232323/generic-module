68ab0a03dee6ec78339bb1ca617812272e119401
cl-cassanelli <cldevofficial@gmail.com>
2024-11-17 20:08:09 +0100

    update localStorage

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,9 +2,11 @@ import head from './head';
 import pinia from './pinia';
 import router from './router';
 import vuetify from './vuetify';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 
 export function registerPlugins(app) {
+  pinia.use(piniaPluginPersistedstate);
   app.
     use(head).
     use(pinia).
--- src/stores/address.js
+++ src/stores/address.js
@@ -2,24 +2,19 @@ import { defineStore } from 'pinia';
 
 export const useAddressStore = defineStore('address', {
   state: () => ({
-    firstname: localStorage.getItem('firstname') || '',
-    lastname: localStorage.getItem('lastname') || '',
-    region: localStorage.getItem('region') || '',
-    province: localStorage.getItem('province') || '',
-    city: localStorage.getItem('city') || '',
-    address: localStorage.getItem('address') || ''
+    firstname: '',
+    lastname: '',
+    region: '',
+    province: '',
+    city: '',
+    address: '',
   }),
   actions: {
     updateField(field, value) {
       this[field] = value;
-      this.saveToLocalStorage(field, value);
-    },
-    saveToLocalStorage(field, value) {
-      localStorage.setItem(field, value);
     },
     clearAll() {
       this.resetState();
-      this.clearLocalStorage();
     },
     resetState() {
       this.firstname = '';
@@ -29,16 +24,23 @@ export const useAddressStore = defineStore('address', {
       this.city = '';
       this.address = '';
     },
-    clearLocalStorage() {
-      const fields = ['firstname', 'lastname', 'region', 'province', 'city', 'address'];
-      fields.forEach(field => localStorage.removeItem(field));
-    },
     getFullAddress() {
-      var fullAddress = `${this.firstname} ${this.lastname}, ${this.address}, ${this.city}, ${this.province}, ${this.region}`;
+      const fullAddress = `${this.firstname} ${this.lastname}, ${this.address}, ${this.city}, ${this.province}, ${this.region}`;
       if (this.firstname && this.lastname && this.address && this.city && this.province && this.region) {
         this.clearAll();
         return fullAddress;
-      } else return false;
-    }
-  }
+      } else {
+        return false;
+      }
+    },
+  },
+  persist: {
+    enabled: true,
+    strategies: [
+      {
+        key: 'addressStore',
+        storage: localStorage,
+      },
+    ],
+  },
 });
