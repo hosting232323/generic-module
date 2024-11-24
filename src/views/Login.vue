b5ff8680ee64ea9e2e1f7046365f12e05fa6eaf1
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2024-11-24 16:39:04 +0100

    refactor: update Login.vue to use data store and simplify props; add login route to router

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,10 @@
 <template>
   <AuthManager
-    :logo="logo"
-    :title="title"
-    :primaryColor="primaryColor"
-    :secondaryColor="secondaryColor"
-    :redirectLink="redirectLink"
+    logo="/logo.png"
+    title="Login"
+    :primaryColor="data.info.primaryColor"
+    :secondaryColor="data.info.secondaryColor"
+    redirectLink="/blog-admin"
     :hostname="hostname"
     :signUp="false"
   />
@@ -12,12 +12,10 @@
 
 <script setup>
   import { AuthManager } from 'generic-module';
+  import { useDataStore } from '@/stores/data';
+  import { storeToRefs } from 'pinia';
 
-  const logo = '/logo.png';
-  const title = 'Login';
-  const primaryColor = '#3498db';
-  const secondaryColor = '#2ecc71';
-  const redirectLink = '/blog-admin'; //** */
-
+  const dataStore = useDataStore();
+  const { data } = storeToRefs(dataStore);
   const hostname = import.meta.env.VITE_HOSTNAME_GENERICBACKED;
 </script>
