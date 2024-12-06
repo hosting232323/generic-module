6a6e3c8462c11a542049ff28291f155f6648e496
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-12-06 16:37:55 +0000

    Generic Login

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import AuthManager from './components/users/AuthManager.vue';
+import Password from './components/users/Password.vue';
+
+export { AuthManager, Password };
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/index.js'), // Correggi qui il percorso
+      name: 'generic-module',
+      fileName: (format) => `generic-module.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue', 'crypto-js', 'vue-router'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'crypto-js': 'CryptoJS',
+          'vue-router': 'VueRouter',
+        },
+      },
+    },
+  },
+});
