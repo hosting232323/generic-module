12b942e537862e4f67bcdf2820bf6f622d4d1594
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-01-25 20:22:04 +0100

    fix login icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+import '@mdi/font/css/materialdesignicons.css';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
