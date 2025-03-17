dd6c2b0838617baf4ecfc189381677be1b6b6ae0
Omar Balde <116875415+omwcoding@users.noreply.github.com>
2025-03-17 15:56:56 +0100

    final final

--- src/components/users/Login.vue
+++ src/components/users/Login.vue
@@ -147,8 +147,11 @@ const handleCredentialResponse = (response) => {
     { token: jwt },
     (data) => {
       if (data.status === 'ok') {
-        localStorage.setItem('strongbox_session_token', data.session_token);
-        router.push(interpolatePath(props.redirectLink, data));
+          localStorage.setItem('token', data.token);
+          if (data.user_info)
+            for (const info of Object.keys(data.user_info))
+              localStorage.setItem(`user_${info}`, data.user_info[info]);
+          router.push(interpolatePath(props.redirectLink, data));
       } else {
         message.value = data.error;
       }
