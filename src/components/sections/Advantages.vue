6d7a3a6548793eb8f207e0fcc4ac215175d25be8
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-03-28 01:35:33 +0100

    generic-sections

--- src/components/sections/Advantages.vue
+++ src/components/sections/Advantages.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container>
-    <h1 :style="{ color: info.primaryColor }">
-      I nostri vantaggi
+    <h1 :style="{ color: props.primaryColor }">
+      {{ props.title }}
     </h1>
-    <v-card elevation="20" v-for="advantage in content" class="margin_top__default">
+    <v-card elevation="20" v-for="advantage in props.content" class="margin_top__default">
       <v-img :src="advantage.image" style="max-height: 600px;" cover />
       <v-card-title style="white-space: normal;">
         {{ advantage.name }}
@@ -17,4 +17,24 @@
 
 <script setup>
   const { content, info } = defineProps(['content', 'info']);
+
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: array,
+      required: true
+    },
+    primaryColor: {
+      type: String,
+      required: true
+    },
+    secondaryColor: {
+      type: String,
+      required: true
+    },
+  })
+
 </script>
