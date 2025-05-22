9f7abad140d668500ab77a7a8b4b4602f8b19750
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-05-22 15:12:16 +0200

    fix-multilingual

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <component
     v-for="section in sections"
     :is="componentMap[section.type]"
-    :id="section.menu ? section.menu.toLowerCase() : null"
+    :id="getText(section.menu) ? getText(section.menu).toLowerCase() : null"
     :content="section.content"
     :info="info"
   />
@@ -21,6 +21,10 @@ import Contacts from '@/components/sections/Contacts';
 import BrandList from '@/components/sections/BrandList';
 import Advantages from '@/components/sections/Advantages';
 import DualSection from '@/components/sections/DualSection';
+
+import { useLanguageStore } from '@/stores/language';
+
+const { getText } = useLanguageStore();
 
 const dataStore = useDataStore();
 const { data } = storeToRefs(dataStore);
