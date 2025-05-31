b2c8ef917a6b4966be96e95ce4bffcd49027237f
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-05-31 22:47:13 +0200

    push

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
-export { default as Map } from '@/components/sections/Map.vue'
-export { default as Line } from '@/components/sections/Line.vue'
-export { default as Gallery } from '@/components/sections/Gallery.vue'
-export { default as Services } from '@/components/sections/Services.vue'
-export { default as Contacts } from '@/components/sections/Contacts.vue'
-export { default as BrandList } from '@/components/sections/BrandList.vue'
-export { default as Advantages } from '@/components/sections/Advantages.vue'
-export { default as DualSection } from '@/components/sections/DualSection.vue'
+export { default as Map } from './sections/Map.vue';
+export { default as Line } from './sections/Line.vue';
+export { default as Gallery } from './sections/Gallery.vue';
+export { default as Services } from './sections/Services.vue';
+export { default as Contacts } from './sections/Contacts.vue';
+export { default as BrandList } from './sections/BrandList.vue';
+export { default as Advantages } from './sections/Advantages.vue';
+export { default as DualSection } from './sections/DualSection.vue';
 
-export { useMobileUtils } from '@/utils/mobile'
-export { useLanguageStore } from '@/stores/language'
+export { useMobileUtils } from '../utils/mobile';
+export { useLanguageStore } from '../stores/language';
