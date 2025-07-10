38fd5f7fe108503457b528248ba509b0acad3c0e
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-07-10 13:09:42 +0200

    push

--- src/components/sections/Advantages.vue
+++ src/components/sections/Advantages.vue
@@ -1,54 +1,18 @@
 <template>
   <v-container>
     <h1 :style="{ color: info.primaryColor }" v-html="getText(content.title) || 'I nostri vantaggi'"/>
-    <v-card
-      v-for="(advantage, index) in content.advantages"
-      :key="index"
-      class="margin_top__default"
-      elevation="20"
-      :class="{ 'carousel--mobile': isMobile }"
-      :style="{
-        marginTop: '1rem'
-      }"
-    >
-      <div
-        :class="['img-wrapper', { 'mobile-height': isMobile }]"
-        :style="{
-          width: '100%',
-          height: isMobile ? '400px' : '600px',
-          overflow: 'hidden',
-          position: 'relative'
-        }"
-      >
-        <v-carousel
-          v-if="Array.isArray(advantage.image)"
-          show-arrows
-          hide-delimiters
-          :height="isMobile ? '400px' : '600px'"
-          :style="isMobile
-            ? {
-                '--v-btn-width': '35px',
-                '--v-btn-height': '35px',
-                '--v-btn-font-size': '14px'
-              }
-            : {}"
-        >
+    <v-card v-for="(advantage, index) in content.advantages" :key="index" class="margin_top__default" elevation="20" :class="{ 'carousel--mobile': isMobile }">
+      <div :class="['img-wrapper', { 'mobile-height': isMobile }]">
+        <v-carousel v-if="Array.isArray(advantage.image)" show-arrows hide-delimiters :height="isMobile ? '400px' : '600px'">
           <v-carousel-item v-for="(img, imgIndex) in advantage.image" :key="imgIndex">
-            <img
-              :src="resolveImg(img)"
-              style="width: 100%; height: 100%; object-fit: cover; display: block;"
-            />
+            <img :src="resolveImg(img)" class="img"/>
           </v-carousel-item>
         </v-carousel>
-        <img
-          v-else
-          :src="resolveImg(advantage.image)"
-          style="width: 100%; height: 100%; object-fit: cover; display: block;"
-        />
+        <img v-else :src="resolveImg(advantage.image)" class="img"/>
       </div>
-      <v-card-title style="white-space: normal;" v-html="getText(advantage.name)" />
+      <v-card-title style="white-space: normal;" v-html="getText(advantage.name)"/>
       <v-card-text v-if="advantage.description">
-        <div v-html="getText(advantage.description)" />
+        <div v-html="getText(advantage.description)"/>
       </v-card-text>
     </v-card>
   </v-container>
