7695bca3154850cfb6b146b440c57544df5cf561
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-08-14 16:49:30 +0200

    push

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -6,9 +6,9 @@ export const encryptPassword = (password, secretKey, ivString) => {
   const iv = CryptoJS.enc.Utf8.parse(ivString);
 
   const encrypted = CryptoJS.AES.encrypt(password, key, {
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
   });
 
   const ivAndCiphertext = iv.concat(encrypted.ciphertext);
@@ -18,13 +18,13 @@ export const encryptPassword = (password, secretKey, ivString) => {
 
 export const decryptPassword = (encryptedBase64, secretKey) => {
   const key = CryptoJS.enc.Utf8.parse(secretKey);
-  const ivAndCiphertext = CryptoJS.enc.Base64.parse(encryptedBase64);
 
-  const iv = CryptoJS.lib.WordArray.create(ivAndCiphertext.words.slice(0, 4)); // 4 words = 16 bytes
+  const ivAndCiphertext = CryptoJS.enc.Base64.parse(encryptedBase64);
+  const iv = CryptoJS.lib.WordArray.create(ivAndCiphertext.words.slice(0, 4), 16);
   const ciphertext = CryptoJS.lib.WordArray.create(ivAndCiphertext.words.slice(4), ivAndCiphertext.sigBytes - 16);
 
-  const decrypted = CryptoJS.AES.decrypt({ ciphertext: ciphertext }, key, {
-    iv: iv,
+  const decrypted = CryptoJS.AES.decrypt({ ciphertext }, key, {
+    iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
