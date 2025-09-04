d9ff9660e9c904d26e2282eef1270f0f79f4ea5f
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-09-04 12:50:12 +0200

    push

---
+++ src/layouts/demo/BlogLayout.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app  v-if="ready">
+    <AppBar />
+    <UpArrow v-if="!showBubbles" :bottomOffset="showChatty ? 100 : 20"/>
+    <SocialBubbles v-if="showBubbles" :chattyActive="showChatty"/>
+    <v-main :style="backgroundStyle">
+      <Blog />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script setup>
+import UpArrow from '../UpArrow.vue';
+import AppBar from '../AppBar.vue';
+import Footer from '../Footer.vue';
+import Blog from '../../views/Blog.vue';
+
+import SocialBubbles from '../SocialBubbles.vue';
+import { defineProps } from 'vue';
+
+import { storeToRefs } from 'pinia';
+import { onMounted, watch, ref, computed } from 'vue';
+import { useDataStore } from '@/stores/data';
+
+const showChatty = ref(false);
+const showBubbles = ref(false);
+const dataStore = useDataStore();
+const { data, ready } = storeToRefs(dataStore);
+
+const props = defineProps({
+  hostname: String,
+  id: Number
+})
+
+const backgroundStyle = computed(() => {
+  if (data.value.info.backgroundImage) {
+    return {
+      backgroundImage: `url(${data.value.info.backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat'
+    };
+  } else {
+    return {
+      backgroundColor: data.value.info.secondaryColor
+    };
+  }
+});
+
+onMounted(() => {
+  dataStore.initDataByDemoLayout(props.hostname, props.id);
+});
+
+watch(ready, (newValue) => {
+  if (!newValue) return
+
+  const addOn = data.value.addOn;
+  if (addOn && addOn.includes('Chatty')) {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = `https://chatty-be.replit.app/chat-file/js?file=inject&user_id=${data.value.info.chattyId}`;
+    document.body.appendChild(script); 
+    script.onload = () => {
+      showChatty.value = true;
+    };
+    script.onerror = () => {
+      showChatty.value = false;
+    };
+  }
+
+  if(data.value.info.socialBubbles) {
+    showBubbles.value = true;
+  }
+});
+</script>
