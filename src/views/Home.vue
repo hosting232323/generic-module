227ff355cb109a81a99673704658cd364ccea8f1
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-11-10 21:57:24 +0100

    fix di vanni

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,18 +14,17 @@ import { computed } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useDataStore } from '@/stores/data';
 
-import Map from '@/components/sections/Map/MapDefault';
-import Text from '@/components/sections/Text/TextDefault';
-import Line from '@/components/sections/Line/LineDefault';
-import Reviews from '@/components/sections/Reviews/ReviewsDefault';
-import Gallery from '@/components/sections/Gallery/GalleryDefault';
-import Services from '@/components/sections/Services/ServicesDefault';
-import Contacts from '@/components/sections/Contacts/ContactsDefault';
-import BrandList from '@/components/sections/BrandList/BrandListDefault';
-import Advantages from '@/components/sections/Advantages/AdvantagesDefault';
-import ShopSummary from '@/components/sections/ShopSummary/ShopSummaryDefault';
-import BlogSummary from '@/components/sections/BlogSummary/BlogSummaryDefault';
-import DualSection from '@/components/sections/DualSection/DualSectionDefault';
+import Map from '@/components/sections/MapDefault';
+import Text from '@/components/sections/TextDefault';
+import Line from '@/components/sections/LineDefault';
+import Reviews from '@/components/sections/ReviewsDefault';
+import Gallery from '@/components/sections/GalleryDefault';
+import Services from '@/components/sections/ServicesDefault';
+import Contacts from '@/components/sections/ContactsDefault';
+import BrandList from '@/components/sections/BrandListDefault';
+import ShopSummary from '@/components/sections/ShopSummaryDefault';
+import BlogSummary from '@/components/sections/BlogSummaryDefault';
+import DualSection from '@/components/sections/DualSectionDefault';
 
 import AdvantagesDefault from '@/components/sections/Advantages/AdvantagesDefault';
 import AdvantagesBusiness from '@/components/sections/Advantages/AdvantagesBusiness';
@@ -44,29 +43,23 @@ const componentMap = {
   services: Services,
   contacts: Contacts,
   brandlist: BrandList,
-  advantages: Advantages,
-  shopSummary: ShopSummary,
-  blogSummary: BlogSummary,
-  dualSection: DualSection
-};
-
-const variantsMap = {
   advantages: {
     default: AdvantagesDefault,
     business: AdvantagesBusiness,
     creative: AdvantagesCreative,
     ecommerce: AdvantagesEcommerce
-  }
+  },
+  shopSummary: ShopSummary,
+  blogSummary: BlogSummary,
+  dualSection: DualSection
 };
 
 const getComponent = (type, variant) => {
-  if (variant && variantsMap[type]?.[variant]) {
-    return variantsMap[type][variant];
-  }
+  if (variant && componentMap[type]?.[variant])
+    return componentMap[type][variant];
   
-  if (variant && variantsMap[type]?.default) {
-    return variantsMap[type].default;
-  }
+  if (componentMap[type]?.default)
+    return componentMap[type].default;
   
   return componentMap[type];
 };
