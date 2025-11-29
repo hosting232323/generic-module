02c7364f01f2eb7352625b143550854aae2f6564
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-11-29 13:01:13 +0000

    Update main

--- src/stores/order.js
+++ src/stores/order.js
@@ -7,7 +7,12 @@ export const useOrderStore = defineStore('order', {
   }),
   actions: {
     addProduct(product) {
-      const existingProduct = this.products.find(item => item.product === product.product);
+      const existingProduct = this.products.find(item => {
+        if (item.variant && product.variant) {
+          return item.product === product.product && item.variant === product.variant;
+        } 
+        return item.product === product.product;
+      });
       if (existingProduct) existingProduct.quantity += 1;
       else {
         product.quantity = 1;
@@ -15,10 +20,22 @@ export const useOrderStore = defineStore('order', {
       }
     },
     removeProduct(product) {
-      const existingProduct = this.products.find(item => item.product === product.product);
-      if (existingProduct) {
-        if (existingProduct.quantity > 1) existingProduct.quantity -= 1;
-        else this.products = this.products.filter(item => item.product !== product.product);
+      const existingProduct = this.products.find(item => {
+        if (item.variant && product.variant) {
+          return item.product === product.product && item.variant === product.variant;
+        }
+        return item.product === product.product
+      });
+
+      if (existingProduct.quantity > 1) {
+        existingProduct.quantity -= 1;
+      } else {
+        this.products = this.products.filter(item => {
+          if (item.variant && product.variant) {
+            return !(item.product === product.product && item.variant === product.variant);
+          }
+          return !(item.product === product.product);
+        });
       }
     },
     removeAllProduct() {
