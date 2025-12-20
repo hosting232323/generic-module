2d0def49b284a16a239c3b5f4191eaabe52e0619
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-12-20 17:05:37 +0100

    push

---
+++ src/layouts/demo/PostLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseLayout>
+    <Post />
+  </BaseLayout>
+</template>
+
+<script setup>
+import Post from '@/views/Post.vue';
+import BaseLayout from '@/layouts/BaseLayout.vue';
+
+import { useDataStore } from '@/stores/data';
+import { onMounted, defineProps } from 'vue';
+
+const props = defineProps({
+  hostname: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: [String, Number],
+    required: true
+  }
+});
+
+const dataStore = useDataStore();
+onMounted(() => dataStore.initDataByDemoOrTemplateLayout(props.hostname, props.type, props.id));
+</script>
