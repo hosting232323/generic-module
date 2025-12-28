9393667c696a285072ad11567f91e39db6586de2
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-12-28 20:01:54 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import '@mdi/font/css/materialdesignicons.css';
 
-export { default as InteractiveDemoLayout } from './layouts/demo/InteractiveDemoLayout.vue';
+// Utenti e Login
+export { encryptPassword, decryptPassword } from './utils/encrypt.js';
+export { default as Password } from './components/users/Password.vue';
+export { default as AuthManager } from './components/users/AuthManager.vue';
 
+// Base Demo Home Page
 export { default as DemoHomeLayout } from './layouts/demo/HomeLayout.vue';
+
+// Add On: Blog
 export { default as DemoPostLayout } from './layouts/demo/PostLayout.vue';
 export { default as DemoBlogLayout } from './layouts/demo/BlogLayout.vue';
+
+// Add On: Menu
 export { default as DemoMenuLayout } from './layouts/demo/MenuLayout.vue';
+
+// Add On: Shop
 export { default as DemoShopLayout } from './layouts/demo/ShopLayout.vue';
-export { default as DemoBookingLayout } from './layouts/demo/BookingLayout.vue';
-export { default as DemoEventDetailsLayout } from './layouts/demo/EventDetailsLayout.vue';
 export { default as DemoProductDetailsLayout } from './layouts/demo/ProductDetailsLayout.vue';
 
-export { default as AuthManager } from './components/users/AuthManager.vue';
-export { default as Password } from './components/users/Password.vue';
+// Add On: Booking
+export { default as DemoBookingLayout } from './layouts/demo/BookingLayout.vue';
+export { default as DemoEventDetailsLayout } from './layouts/demo/EventDetailsLayout.vue';
 
-export { encryptPassword, decryptPassword } from './utils/encrypt.js';
+// Demo Interattiva per Seller Dashboard
+export { default as InteractiveDemoLayout } from './layouts/demo/InteractiveDemoLayout.vue';
