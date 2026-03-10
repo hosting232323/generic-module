5e687a23b9bab3149f244a784a0702f3c389ee4f
cl_cassanelli <cldevofficial@gmail.com>
2026-03-10 16:52:30 +0100

    fix

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-require('dotenv').config();
-const path = require('path');
-const express = require('express');
+import 'dotenv/config';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (_req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
