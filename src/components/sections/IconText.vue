6a1bf0077931b577dca0967f89a141a5fd7f43e2
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-04-04 17:34:01 +0200

    push

--- src/components/sections/IconText.vue
+++ src/components/sections/IconText.vue
@@ -2,17 +2,44 @@
   <v-container>
     <br><h1 :style="{ color: info.primaryColor }">
       <v-row>
-        <v-col cols="12" md="4">
-          <v-icon icon="mdi-food-steak" :color="info.primaryColor" style="float: left;" />
-          <p class="contact__text">&nbsp;&nbsp;Macelleria</p>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-icon
+            icon="mdi-food-steak"
+            :color="info.primaryColor"
+            style="float: left;"
+          />
+          <p class="contact__text">
+            &nbsp;&nbsp;Macelleria
+          </p>
         </v-col>
-        <v-col cols="12" md="4">
-          <v-icon icon="mdi-food-drumstick-outline" :color="info.primaryColor" style="float: left;" />
-          <p class="contact__text">&nbsp;&nbsp;Salumeria</p>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-icon
+            icon="mdi-food-drumstick-outline"
+            :color="info.primaryColor"
+            style="float: left;"
+          />
+          <p class="contact__text">
+            &nbsp;&nbsp;Salumeria
+          </p>
         </v-col>
-        <v-col cols="12" md="4">
-          <v-icon icon="mdi-egg-outline" :color="info.primaryColor" style="float: left;" />
-          <p class="contact__text">&nbsp;&nbsp;Polleria</p>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-icon
+            icon="mdi-egg-outline"
+            :color="info.primaryColor"
+            style="float: left;"
+          />
+          <p class="contact__text">
+            &nbsp;&nbsp;Polleria
+          </p>
         </v-col>
       </v-row>
     </h1>
@@ -20,5 +47,5 @@
 </template>
 
 <script setup>
-  const { content, info } = defineProps(['content', 'info']);
+const { content, info } = defineProps(['content', 'info']);
 </script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -79,57 +79,57 @@ const sections = computed(() => {
 
 
 useHead({
-  title: "Macelleria Colasanto Luigi - Carne di Qualità a Molfetta | Consegna a Domicilio e Assistenza Speciale",
+  title: 'Macelleria Colasanto Luigi - Carne di Qualità a Molfetta | Consegna a Domicilio e Assistenza Speciale',
   meta: [
     {
-      name: "description",
+      name: 'description',
       content:
-        "Macelleria Colasanto Luigi a Molfetta: carne bovina, suina, pollo e formaggi di alta qualità con servizio di consegna a domicilio. Tradizione e gusto dal 1960.",
+        'Macelleria Colasanto Luigi a Molfetta: carne bovina, suina, pollo e formaggi di alta qualità con servizio di consegna a domicilio. Tradizione e gusto dal 1960.',
     },
     {
-      name: "keywords",
+      name: 'keywords',
       content:
-        "macelleria, carne bovina, carne suina, pollo, consegna a domicilio, Molfetta, tradizione, salumi, formaggi, servizio a domicilio, tagli speciali, assistenza culinaria, qualità, artigianato, prodotti freschi",
+        'macelleria, carne bovina, carne suina, pollo, consegna a domicilio, Molfetta, tradizione, salumi, formaggi, servizio a domicilio, tagli speciali, assistenza culinaria, qualità, artigianato, prodotti freschi',
     },
-    { name: "author", content: "Macelleria Colasanto Luigi" },
-    { name: "robots", content: "index, follow" },
-    { name: "viewport", content: "width=device-width, initial-scale=1.0" },
-    { name: "theme-color", content: "#D33F49" },
-    { property: "og:title", content: "Macelleria Colasanto Luigi - Carne di Qualità a Molfetta | Consegna a Domicilio e Assistenza Speciale" },
+    { name: 'author', content: 'Macelleria Colasanto Luigi' },
+    { name: 'robots', content: 'index, follow' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+    { name: 'theme-color', content: '#D33F49' },
+    { property: 'og:title', content: 'Macelleria Colasanto Luigi - Carne di Qualità a Molfetta | Consegna a Domicilio e Assistenza Speciale' },
     {
-      property: "og:description",
+      property: 'og:description',
       content:
-        "Scopri la qualità della Macelleria Colasanto Luigi a Molfetta. Offriamo carne bovina, suina, pollo e salumi con un servizio eccellente e consegna a domicilio.",
+        'Scopri la qualità della Macelleria Colasanto Luigi a Molfetta. Offriamo carne bovina, suina, pollo e salumi con un servizio eccellente e consegna a domicilio.',
     },
-    { property: "og:type", content: "website" },
-    { property: "og:url", content: "https://macelleriacolasanto.it" },
-    { property: "og:image", content: "https://macelleriacolasanto.it/advantages2.jpg" },
-    { property: "og:locale", content: "it_IT" },
-    { property: "og:site_name", content: "Macelleria Colasanto Luigi" },
-    { name: "twitter:card", content: "summary_large_image" },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://macelleriacolasanto.it' },
+    { property: 'og:image', content: 'https://macelleriacolasanto.it/advantages2.jpg' },
+    { property: 'og:locale', content: 'it_IT' },
+    { property: 'og:site_name', content: 'Macelleria Colasanto Luigi' },
+    { name: 'twitter:card', content: 'summary_large_image' },
     {
-      name: "twitter:title",
-      content: "Macelleria Colasanto Luigi - Carne di Qualità a Molfetta | Consegna a Domicilio e Assistenza Speciale",
+      name: 'twitter:title',
+      content: 'Macelleria Colasanto Luigi - Carne di Qualità a Molfetta | Consegna a Domicilio e Assistenza Speciale',
     },
     {
-      name: "twitter:description",
+      name: 'twitter:description',
       content:
-        "Macelleria Colasanto Luigi: carne fresca e di alta qualità a Molfetta con consegna a domicilio. Scopri il nostro assortimento!",
+        'Macelleria Colasanto Luigi: carne fresca e di alta qualità a Molfetta con consegna a domicilio. Scopri il nostro assortimento!',
     },
-    { name: "twitter:image", content: "https://macelleriacolasanto.it/advantages2.jpg" },
-    { name: "twitter:site", content: "@macelleriacolasanto" },
-    { name: "geo.region", content: "IT-BA" }, // Codice della regione per Bari, Puglia
-    { name: "geo.placename", content: "Molfetta" }, // Nome del luogo
-    { name: "geo.position", content: "41.200482;16.598575" }, // Coordinate della posizione
-    { name: "ICBM", content: "41.200482, 16.598575" }, // Obsoleto ma ancora utilizzato da alcuni servizi
-    { name: "business:contact_data:street_address", content: "Piazza Principe di Napoli, 7" },
-    { name: "business:contact_data:locality", content: "Molfetta" },
-    { name: "business:contact_data:region", content: "BA" },
-    { name: "business:contact_data:postal_code", content: "70056" },
-    { name: "business:contact_data:country_name", content: "Italy" },
-    { name: "business:contact_data:email", content: "macelleriacolasanto@libero.it" },
-    { name: "business:contact_data:phone_number", content: "+39 080 397 4062" },
-    { name: "business:contact_data:website", content: "https://macelleriacolasanto.it" }
+    { name: 'twitter:image', content: 'https://macelleriacolasanto.it/advantages2.jpg' },
+    { name: 'twitter:site', content: '@macelleriacolasanto' },
+    { name: 'geo.region', content: 'IT-BA' }, // Codice della regione per Bari, Puglia
+    { name: 'geo.placename', content: 'Molfetta' }, // Nome del luogo
+    { name: 'geo.position', content: '41.200482;16.598575' }, // Coordinate della posizione
+    { name: 'ICBM', content: '41.200482, 16.598575' }, // Obsoleto ma ancora utilizzato da alcuni servizi
+    { name: 'business:contact_data:street_address', content: 'Piazza Principe di Napoli, 7' },
+    { name: 'business:contact_data:locality', content: 'Molfetta' },
+    { name: 'business:contact_data:region', content: 'BA' },
+    { name: 'business:contact_data:postal_code', content: '70056' },
+    { name: 'business:contact_data:country_name', content: 'Italy' },
+    { name: 'business:contact_data:email', content: 'macelleriacolasanto@libero.it' },
+    { name: 'business:contact_data:phone_number', content: '+39 080 397 4062' },
+    { name: 'business:contact_data:website', content: 'https://macelleriacolasanto.it' }
   ]
 });
 
