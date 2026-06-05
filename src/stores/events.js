9054bbe68d2b722129a0da06b44eae2520774261
cl_cassanelli <cldevofficial@gmail.com>
2026-06-05 11:21:22 +0200

    push

--- src/stores/events.js
+++ src/stores/events.js
@@ -1,4 +1,3 @@
-import http from '@/utils/http';
 import { defineStore } from 'pinia';
 
 export const useEventsStore = defineStore('events:genericFeStore', {
@@ -25,47 +24,8 @@ export const useEventsStore = defineStore('events:genericFeStore', {
     setEventsFromJson(events) {
       this.events = events;
     },
-    initEvents(events = undefined) {
-      if(events) this.setEventsFromJson(events);
-      else http.getRequest('event', {}, this.setEvents);
-    },
-
-    async bookEvent(bookingData) {
-      return new Promise((resolve) => {
-        const requestData = {
-          event_id: bookingData.event_id,
-          slot_id: bookingData.slot_id,
-          email: bookingData.email,
-          date: bookingData.date,
-          time: bookingData.time,
-          participants: bookingData.participants ? Number(bookingData.participants) : 1,
-          enrichment: bookingData.enrichment
-        };
-
-        http.postRequest(
-          'booking?project=museocivicobari.it', 
-          requestData, 
-          (response) => {
-            if (response.status == 'ko')
-              resolve({error: response.message});
-            else {
-              const booking = {
-                id: response.id || Date.now(),
-                event_id: bookingData.event_id,
-                slot_id: bookingData.slot_id,
-                email: bookingData.email,
-                participants: bookingData.participants,
-                enrichment: bookingData.enrichment,
-                event: this.getEventById(bookingData.event_id)
-              };
-              
-              this.bookings.push(booking);
-              this.lastBooking = booking;
-              resolve(booking);
-            }
-          }
-        );
-      });
+    initEvents(events) {
+      this.setEventsFromJson(events);
     }
   }
 });
