f3b98f607df0c3ba3d963f3cf7d88b229c1ab939
cl_cassanelli <cldevofficial@gmail.com>
2026-07-27 15:20:39 +0200

    push

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ export { default as validation } from './utils/validation.js';
 // Gestione Estensioni File
 export { default as fileUtils } from './utils/files.js';
 
-// Http Client
-export { createHttpClient } from './utils/http.js';
-
 // Utenti e Login
 export { encryptPassword, decryptPassword } from './utils/encrypt.js';
 export { default as Password } from './components/users/UserPassword.vue';
